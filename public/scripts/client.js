5988990b6270f7ade86e3fb100a995f351652c68
VincentHChoy <vhchoy@ualberta.ca>
2022-05-13 19:40:03 +0000

    moves functions from client.js to helperFunctions.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,85 +1,20 @@
-// const { render } = require("express/lib/response");
-$(document).ready(function() {
-  //creates the html template with personalized user data
-  const createTweetElement = function(data) {
-    const user = data.user;
-    const date = timeago.format(data.created_at);
+import { loadTweets, isValid, createTweetElement } from './helperFunctions.js';
 
-    //escape HTML characters
-    const escape = function(str) {
-      let div = document.createElement("div");
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    };
-
-    const text = escape(data.content.text);
-
-    const markup = `
-          <article class="main-tweet">
-          <header class="main-tweet-head">
-            <div class="username">
-              <img src= ${user.avatars} />
-              <span> ${user.name} </span>
-            </div>
-            <span class="handle"> ${user.handle}</span>
-          </header>
-          <p class="tweet">
-            ${text}
-          </p>
-          <footer class="main-tweet-footer">
-            <span> ${date}</span>
-            <div class="icons">
-              <i class="fa-solid fa-flag"></i>
-              <i class="fa-solid fa-retweet"></i>
-              <i class="fa-solid fa-heart"></i>
-            </div>
-          </footer>
-        </article>`;
-
-    return markup;
-  };
-
-  //validation of text area
-  const isValid = function(text) {
-    let valid = true;
-    newText = text.split("=")[1];
-    let message = "";
-    console.log("newText", newText);
-    if (newText.length > 140) {
-      message = "maximum 140 characters";
-      valid = false;
-    }
-    if (newText.length <= 0) {
-      message = "message empty";
-      valid = false;
-    }
-    if (newText === null || newText === undefined) {
-      message = "undefined";
-      valid = false;
-    }
-
-    return { valid, message };
-  };
-
-  //appends to html container from tweet database
-  const renderTweets = function(database) {
-    for (const user of database.reverse()) {
-      $("#tweets-container").append(createTweetElement(user));
-    }
-  };
+$(document).ready(function () {
 
   //sends text it to the database post route, submit event handler
-  $(".tweet-form").submit(function(event) {
+  $(".tweet-form").submit(function (event) {
     $(".error").slideUp();
     let serialized = $(this).serialize();
     event.preventDefault();
     const validation = isValid(serialized);
+    //handles validation and error messages
     if (!validation.valid) {
-      console.log(validation.message);
       $(".error").text(validation.message).then($(".error").slideDown());
     } else {
       $.post("/tweets", serialized, () => {
         $.get("/tweets").then((data) => {
+          //gets last element in database
           $("#tweets-container").prepend(createTweetElement(data.slice(-1)[0]));
           $("#tweet-composer").val("");
         });
@@ -87,25 +22,21 @@ $(document).ready(function() {
     }
   });
 
-  const loadTweets = function() {
-    $.ajax("/tweets", { method: "GET" }).then(function(tweets) {
-      renderTweets(tweets);
-    });
-  };
-
   //loads the tweets onto the page without redirecting
   loadTweets();
+
+  //hides initial error container
   $(".error").hide();
 
-  /*--Navbar Toggle--*/
-  $(".navbar-menu").click(function() {
+  /*--Navbar Toggle, click event handler--*/
+  $(".navbar-menu").click(function () {
     $(".new-tweet").slideToggle();
     $(".tweet-input").focus();
   });
 
   /*--event handler for handling scrolling--*/
 
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop()) {
       $(".toTop").fadeIn();
     } else {
@@ -113,10 +44,8 @@ $(document).ready(function() {
     }
   });
 
-  
-
   /*--animates going to the top of the screen on click--*/
-  $(".toTop").click(function() {
+  $(".toTop").click(function () {
     $("html, body").animate({ scrollTop: 0 }, 1000);
   });
 });
---
+++ public/scripts/helperFunctions.js
@@ -0,0 +1,72 @@
+//escape HTML characters
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+//creates tweet html
+export const createTweetElement = function (data) {
+  const user = data.user;
+  const date = timeago.format(data.created_at);
+  const text = escape(data.content.text);
+  const markup = `
+          <article class="main-tweet">
+          <header class="main-tweet-head">
+            <div class="username">
+              <img src= ${user.avatars} />
+              <span> ${user.name} </span>
+            </div>
+            <span class="handle"> ${user.handle}</span>
+          </header>
+          <p class="tweet">
+            ${text}
+          </p>
+          <footer class="main-tweet-footer">
+            <span> ${date}</span>
+            <div class="icons">
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>`;
+
+  return markup;
+};
+
+//validation of text area
+export const isValid = function (text) {
+  let valid = true;
+  newText = text.split("=")[1];
+  let message = "";
+  console.log("newText", newText);
+  if (newText.length > 140) {
+    message = "maximum 140 characters";
+    valid = false;
+  }
+  if (newText.length <= 0) {
+    message = "message empty";
+    valid = false;
+  }
+  if (newText === null || newText === undefined) {
+    message = "undefined";
+    valid = false;
+  }
+
+  return { valid, message };
+};
+
+//appends to html container from tweet database
+const renderTweets = function (database) {
+  for (const user of database.reverse()) {
+    $("#tweets-container").append(createTweetElement(user));
+  }
+};
+
+//ajax "get" method for fetching tweets
+export const loadTweets = function () {
+  $.ajax("/tweets", { method: "GET" }).then(function (tweets) {
+    renderTweets(tweets);
+  });
+};
